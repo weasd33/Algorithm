ed9e2bc5ea073a334ffa228afd5252295109e493
SHIM <weasd33@naver.com>
2022-07-15 18:59:21 +0900

    백준 1000, 1001번

---
+++ Algorithm/src/BaekJoon/No1000.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+/*
+	문제
+	두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+	입력
+	첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+	출력
+	첫째 줄에 A+B를 출력한다.
+*/
+public class No1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println("A + B = " + (a + b));
+	}
+}
